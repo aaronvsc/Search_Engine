11c2b84498bb58d63c2ac72229b81b67a0aef8e4
Hunter C Ryan <hcryan@genuse60.smu.edu>
2024-04-10 16:02:56 -0500

    Working on DocumentParser

--- test_DocumentParser.cpp
+++ test_DocumentParser.cpp
@@ -2,12 +2,12 @@
 #include "DocumentParser.cpp"
 #include "catch2/catch.hpp"
 
-TEST_CASE("Stop words are removed", "[stopwords]") {
-    DocumentParser parser;
-    std::string text = "This is the test of the stop word removal";
+//TEST_CASE("Stop words are removed", "[stopwords]") {
+    //DocumentParser parser;
+    //std::string text = "This is the test of the stop word removal";
     //std::string processedText = parser.removeStopWords(text);
     //REQUIRE(processedTest == "This test stop word removal");
-}
+//}
 TEST_CASE("Words stemmed correctly", "[stemming]") {
     std::string word = "running";
     std::string stemmedWord = DocumentParser::stemWord(word);
