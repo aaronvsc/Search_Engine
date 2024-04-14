26ba9422704af97bc69dd9053e9d5c360b1168a7
SpencerNeill <sneill@smu.edu>
2024-04-14 17:25:25 -0500

    Continued Work on DocumentParser

--- DocumentParser.h
+++ DocumentParser.h
@@ -1,12 +1,19 @@
 #ifndef DOCUMENT_PARSER_H
 #define DOCUMENT_PARSER_H
 #include "Porter2/porter2Stemmer.cpp"
+#include "AvlTree.h"
 #include <iostream>
 #include <set>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <cctype>
+#include <iomanip>
+#include <filesystem>
+
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/document.h"
+using namespace rapidjson;
 
 class DocumentParser {
 
@@ -24,11 +31,11 @@ public:
 
         if(!file.is_open()) {
             std::cerr << "Unable to open file: " << filePath << std::endl;
-            return;
         }
         while (file >> word) {
             stopWords.insert(word);
         }
+
         file.close();
     }
 
@@ -72,9 +79,60 @@ public:
         return word;
     }
 
-    //runDocument()
-    //pushToTree()
-    //relevancy()
+    void runDocument(std::string documentName) {
+        //remember to do persons and organizations later
+
+        std::ifstream input(documentName);
+        if (!input.is_open())
+        {
+            std::cerr << "cannot open file: " << documentName << std::endl;
+            return;
+        }
+
+        IStreamWrapper isw(input);
+        Document d;
+        d.ParseStream(isw);
+
+        std::string docText = d["text"].GetString();
+        std::cout << docText << std::endl;
+
+        std::vector<std::string> tokens = tokenizer(docText);
+
+        for (int i = 0; i < tokens.size(); i++) {
+            tokens[i] = removePunctuation(tokens[i]);
+            tokens[i] = toLower(tokens[i]);
+            tokens[i] = stemWord(tokens[i]);
+        }
+
+        for (int i = 0; i < tokens.size(); i++) {
+            if (!containsStopWords(tokens[i])) {
+                pushToTree(tokens[i], documentName, relevancy(tokens[i], tokens));
+            }
+        }
+
+        input.close();
+    }
+
+
+    void pushToTree(std::string token, std::string docName, double frequency) {
+
+        //insert(token, docName, frequency)
+
+    }
+
+
+    double relevancy(std::string word, std::vector<std::string> tokens) {
+        double appearancePercent = 0.00;
+        int counter, appearanceNum = 0;
+        for (int i = 0; i < tokens.size(); i++) {
+            if (word == tokens.at(i)) {
+                appearanceNum++;
+            }
+            counter++;
+        }
+        appearancePercent = appearanceNum / counter;
+        return appearancePercent;
+    }
 
 };
 std::set<std::string> DocumentParser::stopWords;
