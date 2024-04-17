6f643e19f761eb6d432b652d385953041c84413d
SpencerNeill <sneill@smu.edu>
2024-04-17 17:20:43 -0500

    Start of Query Processor

---
+++ QueryProcessor.h
@@ -0,0 +1,28 @@
+#include "AvlTree.h"
+#include "DocumentParser.h"
+#include <map>
+
+
+// SeperateString()
+// findWord()
+// findPerson()
+// find Person()
+
+std::map<std::string, std::vector<std::string>> documents;
+
+std::vector<std::string> SeperateStirng(std::string search) {
+    // 
+}
+
+std::vector<std::string> findWord(std::string wordToFind) {
+
+}
+
+std::vector<std::string> findPerson(std::string personToFind) {
+
+}
+
+std::vector<std::string> findOrganization(std::string orgToFind) {
+
+}
+
