fdaccd90213eb6e33596f51d0cbf055f3a6419bb
aaronvsc <aaronvsc@outlook.com>
2024-04-25 14:35:38 -0500

    Working Query Processor

--- QueryProcessor.h
+++ QueryProcessor.h
@@ -1,133 +1,144 @@
+
 #ifndef QUERY_PROCESSOR_H
 #define QUERY_PROCESSOR_H
-#include <map>
 #include <cmath>
+#include <map>
 
 #include "AvlTree.h"
 #include "DocumentParser.h"
 
-// SeperateString()
-// findWord()
-// findPerson()
-// find Person()
 class QueryProcessor {
-    private :
+   private:
     AvlTree<std::string>& PersonTree;
-    AvlTree<std::string> &OrganizationTree;
+    AvlTree<std::string>& OrganizationTree;
     AvlTree<std::string>& WordsTree;
+
+    // vector of maps to compare
+    std::vector<std::map<std::string, int>> vectorOfMaps;
+    std::vector<std::map<std::string, int>> vectorOfBadMaps;
+
    public:
+    QueryProcessor(AvlTree<std::string>& person, AvlTree<std::string>& org, AvlTree<std::string>& word) : PersonTree(person), OrganizationTree(org), WordsTree(word) {}
 
-   QueryProcessor(AvlTree<std::string> &person, AvlTree<std::string> &org, AvlTree<std::string> &word):PersonTree(person),OrganizationTree(org),WordsTree(word){
-        // PersonTree = person;
-        // OrganizationTree = org;
-        // WordsTree = word;
-   }
+    // Function to prcoess query, returns final map of desired search results
+    std::map<std::string, int> processQuery(std::string search) {
+        SeperateString(search);
 
-    std::map<std::string, std::vector<std::string>> documents;
-    std::string tempWord = "";
+        if (vectorOfMaps.empty()) {
+            return {};  // Return an empty map if there are no input maps
+        }
 
-    std::map<std::string, std::vector<std::string>> SeperateStirng(std::string search) {
-        std::vector<std::string> wordsToSearch = tokenizer(search);
+        std::map<std::string, int> result = vectorOfMaps[0];  // Start with the first map
+
+        // Find the intersection of all maps
+        for (size_t i = 1; i < vectorOfMaps.size(); ++i) {
+            result = intersectMaps(result, vectorOfMaps[i]);
+        }
+
+        // Exclude documents specified in badMaps
+        for (const auto& badMap : vectorOfBadMaps) {
+            result = excludeMaps(result, badMap);
+        }
+
+        return result;
+    }
+
+    void outputTopDocuments(const std::map<std::string, int>& documentFrequencyMap) {
+        // Create a vector of pairs to hold the documents and their frequencies
+        std::vector<std::pair<std::string, int>> documentFrequencyPairs;
+
+        // Copy the content of the map to the vector of pairs
+        for (const auto& pair : documentFrequencyMap) {
+            documentFrequencyPairs.push_back(pair);
+        }
+
+        // Sort the vector of pairs based on the frequency counts in descending order
+        std::sort(documentFrequencyPairs.begin(), documentFrequencyPairs.end(),
+                  [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+                      return a.second > b.second;  // Sort in descending order of frequency counts
+                  });
+
+        // Output the first 15 documents along with their frequency counts
+        int count = 0;
+        for (const auto& pair : documentFrequencyPairs) {
+            if (count >= 15) {
+                break;  // Exit the loop after outputting the first 15 documents
+            }
+            // NOTE instead of DOCUMENT ID AND FREQUENCY. WE NEED A FUNCTION TO RETRIEVE ARTICLE NAME, PUBLICATION, and DATE
+            std::cout << count + 1 << ". Document ID: " << pair.first << ", Frequency: " << pair.second << std::endl;
+            ++count;
+        }
+    }
+
+    void SeperateString(std::string search) {
+        std::string tempWord = "";
+        std::vector<std::string> wordsToSearch = DocumentParser::tokenizer(search);
+
+        for (int i = 0; i < wordsToSearch.size(); i++) {
+            wordsToSearch[i] = removePunctuationExcept(wordsToSearch[i]);
+        }
+
+        // for (const std::string& word : wordsToSearch) {
+        //     std::cout << word << std::endl;
+        // }
+
         for (int i = 0; i < wordsToSearch.size(); i++) {
             if (wordsToSearch[i].substr(0, 4) == "ORG:") {
                 tempWord = wordsToSearch[i].substr(4);
-                documents.insert({tempWord, findOrganization(tempWord)});
+                vectorOfMaps.push_back(OrganizationTree.getWordMapAtKey(tempWord));
+
             } else if (wordsToSearch[i].substr(0, 7) == "PERSON:") {
                 tempWord = wordsToSearch[i].substr(7);
-                documents.insert({tempWord, findPerson(tempWord)});
+                vectorOfMaps.push_back(PersonTree.getWordMapAtKey(tempWord));
+
             } else if (wordsToSearch[i].substr(0, 1) == "-") {
-                tempWord = wordsToSearch[i].substr(1);
-                documents.insert({tempWord, notFind(tempWord)});
+                tempWord = DocumentParser::stemWord(wordsToSearch[i].substr(1));
+                vectorOfBadMaps.push_back(WordsTree.getWordMapAtKey(tempWord));
+
+            } else if (!DocumentParser::containsStopWords(wordsToSearch[i]) && wordsToSearch[i] != "") {
+                vectorOfMaps.push_back(WordsTree.getWordMapAtKey(DocumentParser::stemWord(wordsToSearch[i])));
             } else {
-                documents.insert({wordsToSearch[i], findWord(wordsToSearch[i])});
+                continue;
             }
         }
-        return documents;
     }
 
-    std::vector<std::string> findWord(std::string wordToFind) {
-        // search through the AVL tree to find the word and put all the documents related to the word into a vector
-        std::vector<std::string> documents;
-        AvlNode* node = findNode(wordToFind, root);
-        if (node != nullptr) {
-            for (const auto& entry : node->wordMap) {
-                documents.push_back(entry.first);
+    std::map<std::string, int> intersectMaps(std::map<std::string, int>& map1, std::map<std::string, int>& map2) {
+        std::map<std::string, int> intersectMap;
+
+        for (auto it = map1.begin(); it != map1.end(); ++it) {
+            auto findIter = map2.find(it->first);
+            if (findIter != map2.end()) {
+                // Key exists in both maps, add the frequencies together
+                intersectMap[it->first] = it->second + findIter->second;
+                map2.erase(findIter);  // Remove the key from map2
             }
         }
-        return documents;
+
+        return intersectMap;
     }
 
-    std::vector<std::string> findPerson(std::string personToFind) {
-        // search through the AVL tree to find the person and put all the documents related to the person into a vector
-        std::vector<std::string> personDocs;
-        personDocs.push_back(PersonTree.findNode(personToFind))
+    std::map<std::string, int> excludeMaps(const std::map<std::string, int>& map, const std::map<std::string, int>& badMap) {
+        std::map<std::string, int> excludeMap;
 
-    }
-
-    std::vector<std::string> findOrganization(std::string orgToFind) {
-        // search through the AVL tree to find the organization and put all the documents related to the organization into a vector
-    }
-
-    std::vector<std::string> notFind(std::string wordToNotFind) {
-        // find the total number of documents
-        // search through the AVL tree to find the word and put all the documents related to the organization into a vector
-        // compare the two lists and return a vector of the documents that are not in both lists
-    }
-
-    std::vector<std::string> rankRelevancy(std::vector<std::string> docsToRank) {
-        // givens a vector of documents, find rank to relevancy of the documents
-    }
-
-    std::map<std::string, int> calculateTermFrequency(std::string document) {
-        std::vector<std::string> words = tokenizer(document);
-        std::map<std::string, int> freq;
-        for (const auto& word : words) {
-            freq[word]++;
-        }
-        return freq;
-    }
-    std::map<std::string, double> calculateIDF(std::vector<std::string> documents) {
-        std::map<std::string, int> docCount;
-        for (const auto& doc : documents) {
-            std::vector<std::string> words = tokenizer(doc);
-            std::set<std::string> seen;
-            for (const auto& word : words) {
-                if (seen.insert(word).second) {
-                    docCount[word]++;
-                }
+        for (const auto& pair : map) {
+            if (badMap.find(pair.first) == badMap.end()) {
+                // Document ID not found in badMap, include it in the result
+                excludeMap.insert(pair);
             }
         }
-        int totalDocs = documents.size();
-        std::map<std::string, double> idf;
-        for (const auto& [word, count] : docCount) {
-            idf[word] = log(static_cast<double>(totalDocs) / count);
-        }
-        return idf;
-    }
-    std::map<std::string, double> calculateTFIDF(const std::string& document, const std::map<std::string, double>& idf) {
-        std::map<std::string, int> tf = calculateTermFrequency(document);
-        std::map<std::string, double> tfidf;
-        for (const auto& [word, freq] : tf) {
-            if (idf.find(word) != idf.end()) {
-                tfidf[word] = freq * idf.at(word);
-            }
-        }
-        return tfidf;
+
+        return excludeMap;
     }
 
-    static std::vector<std::string> tokenizer(const std::string textLine) {
-        std::string word = "";
-        std::vector<std::string> tokens;
-        int startingLetter = 0;
-        for (int i = 0; i < textLine.length(); i++) {
-            if (textLine.at(i) == ' ') {
-                word = textLine.substr(startingLetter, i);
-                startingLetter = i + 1;
-                tokens.push_back(word);
+    static std::string removePunctuationExcept(std::string word) {
+        for (int i = 0; i < word.length(); ++i) {
+            if (ispunct(word.at(i)) && word.at(i) != ':' && word.at(i) != '-') {
+                word.erase(i, 1);
+                --i;
             }
         }
-        return tokens;
+        return word;
     }
-}
-
+};
 #endif
