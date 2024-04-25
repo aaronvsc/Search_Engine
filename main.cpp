d95cac49a9817711d8c7cb0e79132f46970f547e
Hunter C Ryan <hcryan@genuse60.smu.edu>
2024-04-25 16:52:07 -0500

    Working on main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "QueryProcessor.h"
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
 
     AvlTree<std::string> PersonTree;
@@ -24,5 +24,61 @@ int main()
          << "\tsupersearch ui\n\n";
 
     cout << "supersearch not implemented yet." << endl; 
+
+    std::string command = argv[1];
+    if (command == "index" && argc == 3) {
+        std::string directory = argv[2];
+        indexFiles(directory);
+    }
+    else if (command == "query" && argc == 3) {
+        std::string query = argv[2];
+        processQuery(query);
+    } 
+    else if (command == "ui" && argc == 2) {
+        startUI();
+    } else {
+        std::cerr << "Invalid usage. Correct usage:\n" 
+        << argv[0] << " index <directory>\n" << argv[0] << 
+        " query <query-string>\n" << argv[0] << " ui\n";
+        exit(1);
+    }
+    char userChoice;
+    std::string input;
+
+    while(true) {
+        std::cout<< "\nSuperSearch Menu\n";
+        std::cout<< "Press i to index a directory.\n";
+        std::cout<< "Press s to save the index.\n";
+        std::cout<< "Press l to load the index.\n";
+        std::cout<< "Press q to perform query.\n";
+        std::cout<< "Press e to exit.\n";
+        std::cout<< "Enter the choice: ";
+        std::cin >> userChoice;
+
+        switch(userChoice) {
+            case 'i':
+            
+            case 's':
+
+            case 'l':
+
+            case 'q':
+
+            case 'e':
+
+            default: 
+
+        }
+
+
+
+
+
+        
+
+    }
+
+
+
     return 0;
 }
